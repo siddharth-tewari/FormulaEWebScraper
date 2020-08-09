03c3535dfe50090ab3afd4ea456b52294549c039
Diogo Ribeiro <ribeiropdiogo@gmail.com>
2020-08-09 18:39:24 +0100

    Version 1.0

--- fescraper.py
+++ fescraper.py
@@ -1,4 +1,4 @@
-"""Web scraper developed in Python that scrapes the official FIA Formula E website for driver/team standings and race informations.\n
+"""Web scraper developed in Python that scrapes the official FIA Formula E website for driver/team standings and race informations and stores everything in JSON files.\n
 Usage:
   fescraper.py [<year>]...
   fescraper.py --version
